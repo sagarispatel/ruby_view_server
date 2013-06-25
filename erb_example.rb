0ab8d179f4c4df65a4588e3d5a97f2f081bce6a3
MakerSquare Student <student@lab-5.att.net>
2013-06-25 11:33:25 -0500

    added content to erb_example file

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = %w(awesome cool rad)
+template = ERB.new "This is <%= y %>"
+puts template.result(binding)
+
+z = {name: "sagar", age: "23"}
+b = z.keys
+template = ERB.new "This is <%= b %>"
+puts template.result(binding)
